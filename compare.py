85bf19d82c0a94666bd25d2897959185846b2daa
OfirShitrit <ofirha18@gmail.com>
2024-05-12 14:20:07 +0300

    add mincover

--- compare.py
+++ compare.py
@@ -32,7 +32,7 @@ def solve_with_root(a: np.ndarray, b: np.ndarray):
     array([0., 0.])
     """
 
-    # The function that the program will solve - the fun parameter or root
+    # The function that the program will solve - the fun parameter for root
     def equation(x):
         return np.dot(a, x) - b
 
@@ -47,9 +47,9 @@ def solve_with_root(a: np.ndarray, b: np.ndarray):
 
 
 def test_solve_with_root():
-    num_tests = 5
+    num_tests = 50
     for _ in range(num_tests):
-        n = np.random.randint(2, 6)
+        n = np.random.randint(2, 10)
         a = np.random.rand(n, n)  # np.linalg.solve get only square matrices
         b = np.random.randn(n)
 
@@ -74,38 +74,31 @@ def test_solve_with_root():
             print("Vector b:", b)
             print("Solution (solve_with_root):", solution_root)
             print("Solution (numpy.linalg.solve):", solution_numpy)
-        else:
-            print("Test passed")
+    print("All tests passed!")
 
 
 def compare_solution_methods():
     input_sizes = list(range(1, 1001, 50))
 
-    # Initialize lists to store average running times
     avg_time_solve_with_root = []
     avg_time_numpy_solve = []
 
-    # Perform comparisons for each input size
     for size in input_sizes:
-        # Generate random matrix 'a' and vector 'b'
         a = np.random.randn(size, size)
         b = np.random.randn(size)
 
-        # Measure running time for solve_with_root
         start_time = time.time()
         solve_with_root(a, b)
         end_time = time.time()
-        elapsed_time_solve_with_root = end_time - start_time
-        avg_time_solve_with_root.append(elapsed_time_solve_with_root)
+        time_solve_with_root = end_time - start_time
+        avg_time_solve_with_root.append(time_solve_with_root)
 
-        # Measure running time for numpy.linalg.solve
         start_time = time.time()
         np.linalg.solve(a, b)
         end_time = time.time()
-        elapsed_time_numpy_solve = end_time - start_time
-        avg_time_numpy_solve.append(elapsed_time_numpy_solve)
+        time_linalg = end_time - start_time
+        avg_time_numpy_solve.append(time_linalg)
 
-    # Plot the results
     plt.figure(figsize=(10, 6))
     plt.plot(input_sizes, avg_time_solve_with_root, label='solve_with_root', marker='o')
     plt.plot(input_sizes, avg_time_numpy_solve, label='numpy.linalg.solve', marker='o')
@@ -114,27 +107,11 @@ def compare_solution_methods():
     plt.title('Performance Comparison of Solution Methods')
     plt.legend()
     plt.grid(True)
-    plt.savefig("comparison.png")  # after you plot the graphs, save them to a file and upload it separately.
-    plt.show()
+    plt.savefig("comparison.png")
+    # plt.show()
 
 
 if __name__ == '__main__':
-    # put your code here
-    # test_solve_with_root()
+    test_solve_with_root()
     compare_solution_methods()
-    # a = np.array([[1, 2], [3, 4]])
-    # b = np.array([5, 6])
-    #
-    # a = np.array([[1,8], [1, 0]])
-    # b = np.array([5,2])
 
-    # a = np.array([[1, 8, 3], [1, 0, 4], [4, 7, 5]])
-    # b = np.array([1, 2, 3])
-    # #
-    # # # solution = solve_with_root(a, b)
-    # # # print("root:", solution)
-    # #
-    # try:
-    #     print("linalg:", np.linalg.solve(a, b))
-    # except:
-    #     print("An exception has occurred!")
---
+++ mincover.py
@@ -0,0 +1,58 @@
+import random
+import subprocess, sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxpy"], stdout=subprocess.DEVNULL)
+subprocess.check_call([sys.executable, "-m", "pip", "install", "cvxopt"], stdout=subprocess.DEVNULL)
+
+import networkx as nx, cvxpy, numpy as np, matplotlib.pyplot as plt
+
+def mincover(graph: nx.Graph)->int:
+    """
+    Find the minimum vertex cover size in the graph using integer linear programming
+
+    :param graph: The input graph
+    :return: A number containing the minimum vertex cover size
+    """
+    n = graph.number_of_nodes()
+
+    x = cvxpy.Variable(n, boolean=True)
+    objective = cvxpy.Minimize(cvxpy.sum(x))
+
+    constraints = []
+    for u, v in graph.edges:
+        constraints.append(x[u] + x[v] >= 1)
+
+    problem = cvxpy.Problem(objective, constraints)
+
+    problem.solve()
+
+    return int(np.round(problem.value))
+
+
+def test_mincover():
+    for i in range(10):
+        num_nodes = random.randint(2, 50)
+        num_edges = random.randint(1, 1000)
+        graph = nx.gnm_random_graph(num_nodes, num_edges)
+        min_cover_size = mincover(graph)
+        print(f"Graph {i+1}:")
+        print(f"Number of nodes: {num_nodes}")
+        print(f"Number of arcs: {num_edges}")
+        print(f"Minimum Vertex Cover Size: {min_cover_size}")
+        print()
+
+
+if __name__ == '__main__':
+    edges=eval(input())
+    # edges = [(0, 2), (1, 2)]
+    # edges = [(0, 2), (1, 2), (3,2), (3,1)]
+    # edges = [(0, 1), (2, 3), (4,5)]
+    # edges = [(0, 1), (0, 2), (1, 2)]
+    graph = nx.Graph(edges)
+
+    # # Plot the input graph
+    # plt.figure(figsize=(8, 6))
+    # nx.draw(graph, with_labels=True, node_color='skyblue', node_size=800, font_size=12, font_weight='bold')
+    # plt.title("Input Graph")
+    # plt.show()
+    print(mincover((graph)))
+    # test_mincover()
