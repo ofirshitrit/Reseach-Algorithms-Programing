dc3266b309469e5100b0eff7c2573560a32999ce
OfirShitrit <ofirha18@gmail.com>
2024-05-26 18:22:38 +0300

    -

--- sums.py
+++ sums.py
@@ -1,60 +1,51 @@
-# Put your iterator/generator here.
-# It should be called: sorted_subset_sums.
-import itertools
 import heapq
-
-
-def sorted_subset_sums(series):
-    """
-       Generate the sorted sums of all subsets of the input series.
-
-       :param series: list of different positive numbers.
-       yields: The sum of each subset in ascending order.
-
-        >>> from itertools import takewhile, islice
-
-       # >>> for i in sorted_subset_sums([1,2,4]): print(i, end=", ")
-       # 0, 1, 2, 3, 4, 5, 6, 7,
-
-        >>> list(sorted_subset_sums([1,2,3]))
-        [0, 1, 2, 3, 3, 4, 5, 6]
-
-        >>> list(sorted_subset_sums([2,3,4]))
-        [0, 2, 3, 4, 5, 6, 7, 9]
-
-        >>> list(takewhile(lambda x:x<=6, sorted_subset_sums(range(1,100))))
-        [0, 1, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 6]
-
-        >>> list(zip(range(5), sorted_subset_sums(range(100))))
-        [(0, 0), (1, 0), (2, 1), (3, 1), (4, 2)]
-
-
+from itertools import takewhile, islice
+def sorted_subset_sums(S):
 
     """
-    series = sorted(series)
 
-    def generate_subsets(index, current_sum):
-        if index == len(series):
-            yield current_sum
-            return
+    :param S:
+    :return:
 
-        # Include the current element in the subset
-        yield from generate_subsets(index + 1, current_sum + series[index])
+    >>> from itertools import takewhile, islice
+    >>> for i in sorted_subset_sums([1,2,4]): print(i, end=", ")
+    0, 1, 2, 3, 4, 5, 6, 7,
 
-        # Exclude the current element from the subset
-        yield from generate_subsets(index + 1, current_sum)
+    >>> list(sorted_subset_sums([1,2,3]))
+    [0, 1, 2, 3, 3, 4, 5, 6]
 
-    yield from generate_subsets(0, 0)
+    >>> list(sorted_subset_sums([2,3,4]))
+    [0, 2, 3, 4, 5, 6, 7, 9]
 
+    >>> list(islice(sorted_subset_sums(range(100)),5))
+    [0, 0, 1, 1, 2]
 
-if __name__ == '__main__':
-    from itertools import takewhile, islice
-    # for i in eval(input()):
-    #     print(i, end=", ")
-    # j = 0
-    for i in sorted_subset_sums(range(100)):
-        print(f"Current subset sum: {i} ")
-        # print(i, end=", ")
-    # print(list(islice(sorted_subset_sums(range(100)), 5)))
+    >>> list(takewhile(lambda x:x<=6, sorted_subset_sums(range(1,100))))
+    [0, 1, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 6]
 
-    print(list(takewhile(lambda x: x <= 6, sorted_subset_sums(range(1, 100)))))
+    >>> list(zip(range(5), sorted_subset_sums(range(100))))
+    [(0, 0), (1, 0), (2, 1), (3, 1), (4, 2)]
+
+    >>> len(list(takewhile(lambda x:x<=1000, sorted_subset_sums(list(range(90,100)) + list(range(920,1000))))))
+    1104
+    """
+    S = sorted(S)  # Ensure S is sorted to handle subsets in increasing order
+    n = len(S)
+
+    # Min-heap to store the sums, initialized with the sum of the empty subset
+    heap = [(0, 0)]  # (current_sum, index)
+
+    # A set to track visited (current_sum, index) pairs to avoid duplicates
+    visited = set((0, 0))
+
+    while heap:
+        current_sum, idx = heapq.heappop(heap)
+        yield current_sum
+
+        # Generate new sums by adding subsequent elements from S
+        for i in range(idx, n):
+            new_sum = current_sum + S[i]
+            new_pair = (new_sum, i + 1)
+            if new_pair not in visited:
+                heapq.heappush(heap, new_pair)
+                visited.add(new_pair)
